cec637365e645b9c5cec26a6c8261970d5256f31
abejedires <abejedires@gmail.com>
2024-05-07 22:17:11 -0400

    header and footer

---
+++ src/Utils/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+});
+
+
+
+
+
+// "https://api.themoviedb.org/3/trending/all/day?api_key=bebd8381713dbcba34b312a43f4b1a4c&language=en-US
---
+++ src/Utils/requests.js
@@ -0,0 +1,12 @@
+const API_KEY="ad7512a6620749349b2a03812e0eea12";
+const requests ={
+    fetchTrending:`/trending/all/day?api_key={API_KEY}language=en-US`
+
+
+
+
+
+
+
+} ;
+export default requests
